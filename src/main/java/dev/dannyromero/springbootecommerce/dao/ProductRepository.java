a301fe5f80b615e6e019b69bc560aee920dadf44
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-05-10 17:41:58 -0500

    Updated ProductRepository to allow for category search by category id. The HTTP method now allows for a get request using the following format: http://localhost:8080/api/products/search/findByCategoryId?id=# where # is the category id number.

--- src/main/java/dev/dannyromero/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/dev/dannyromero/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,13 @@
 package dev.dannyromero.springbootecommerce.dao;
 
 import dev.dannyromero.springbootecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
