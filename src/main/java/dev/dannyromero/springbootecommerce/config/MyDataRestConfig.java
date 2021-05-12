1d3a27919aeb6553c29254d7181e245a62508018
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-05-12 18:53:50 -0500

    Updated the REST repository configuration to expose product category ids.

--- src/main/java/dev/dannyromero/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/dev/dannyromero/springbootecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package dev.dannyromero.springbootecommerce.config;
 
 import dev.dannyromero.springbootecommerce.entity.Product;
 import dev.dannyromero.springbootecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,6 +40,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entityTypes = entityManager.getMetamodel().getEntities();
+
+        // These types must be changed into java classes.
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType type : entityTypes)
+            entityClasses.add(type.getJavaType());
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
 }
