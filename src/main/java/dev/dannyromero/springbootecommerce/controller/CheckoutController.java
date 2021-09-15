186fd594e49be8d857da0747fed354131e9f2768
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-09-15 13:17:09 -0500

    Added checkout functionality to CheckoutController.java

--- src/main/java/dev/dannyromero/springbootecommerce/controller/CheckoutController.java
+++ src/main/java/dev/dannyromero/springbootecommerce/controller/CheckoutController.java
@@ -1,23 +1,47 @@
 package dev.dannyromero.springbootecommerce.controller;
 
+import dev.dannyromero.springbootecommerce.dao.CustomerRepository;
 import dev.dannyromero.springbootecommerce.dto.Purchase;
 import dev.dannyromero.springbootecommerce.dto.PurchaseResponse;
-import dev.dannyromero.springbootecommerce.service.CheckoutService;
+import dev.dannyromero.springbootecommerce.entity.Customer;
+import dev.dannyromero.springbootecommerce.entity.Order;
+import dev.dannyromero.springbootecommerce.entity.OrderItem;
 import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
-    private CheckoutService checkoutService;
+    private final CustomerRepository customerRepository;
 
-    public CheckoutController(CheckoutService checkoutService) {
-        this.checkoutService = checkoutService;
+    public CheckoutController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     @PostMapping("/purchase")
+    @Transactional
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
-        return checkoutService.placeOrder(purchase);
+        Order order = purchase.getOrder();
+
+        String orderTrackingNumber = UUID.randomUUID().toString();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(order::add);
+
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        customerRepository.save(customer);
+        return new PurchaseResponse(orderTrackingNumber);
     }
+
 }
--- src/main/java/dev/dannyromero/springbootecommerce/service/CheckoutService.java
+++
@@ -1,8 +0,0 @@
-package dev.dannyromero.springbootecommerce.service;
-
-import dev.dannyromero.springbootecommerce.dto.Purchase;
-import dev.dannyromero.springbootecommerce.dto.PurchaseResponse;
-
-public interface CheckoutService {
-    PurchaseResponse placeOrder(Purchase purchase);
-}
